04ded29e80a229211c05ec91b3ac507ab2339a49
Никита Белов <n.belov@sberauto.com>
2025-08-07 23:04:04 +0300

    debug

--- esmodules/configs/settings.mjs
+++ esmodules/configs/settings.mjs
@@ -4,8 +4,8 @@ import { SYSTEM } from "./system.mjs";
 export const registerSystemSettings = function() {
 
   game.settings.register(SYSTEM.id, 'isSystemDebug', {
-    name: game.i18n.localize("CZT.Common.Debug.Label"),
-    hint: game.i18n.localize("CZT.Common.Debug.Info"),
+    name: game.i18n.localize("DEBUG.Label"),
+    hint: game.i18n.localize("DEBUG.Info"),
     scope: 'world',
     config: true,
     type: Boolean,
